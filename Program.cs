6a3710836fa058831bb8c7fac26370b93864f556
Levente <gecsel@kkszki.hu>
2024-10-18 10:12:30 +0200

    Penzugy project is created

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Gl_Doga_1
+{
+    internal class Program
+    {
+        public static void Osztalyzat()
+        {
+            
+        }
+        static void Main(string[] args)
+        {
+            Osztalyzat();
+        }
+    }
+}
